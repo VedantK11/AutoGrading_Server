4b6ea528306a8b9b0f995ec607bbaf4d9dff6184
VedantK11 <vedantuk11@gmail.com>
2023-11-05 22:19:34 +0530

    Updation in throughput calculation

--- Lab08/lab08_client/gradingclient.cpp
+++ Lab08/lab08_client/gradingclient.cpp
@@ -21,14 +21,14 @@ int main(int argc, char *argv[]) {
     int sleepTimeSeconds = std::atoi(argv[4]);
     int timeoutSeconds = std::atoi(argv[5]);
 
-    double successful_totalTime = 0.0;
-    double totalTime_of_loop = 0.0;
     int successfulResponses = 0;
     double responseTime = 0;
     int totalRequests = loopNum;
-    int successfulRequests = 0;
+    // int successfulRequests = 0;
     int timeoutRequests = 0;
     int errorRequests = 0;
+    double successful_totalTime = 0.0;
+    double totalTime = 0.0;
 
     // Extract server IP and port from the command line argument
     size_t colonPos = serverIPPort.find(':');
@@ -49,6 +49,10 @@ int main(int argc, char *argv[]) {
 
     std::string sourceCodeContent((std::istreambuf_iterator<char>(sourceFile)),
                                   std::istreambuf_iterator<char>());
+
+
+    struct timeval start, end,response_start,response_end;
+    gettimeofday(&start, NULL);
 
     // Perform the request-response loop
     for (int i = 0; i < loopNum; i++) {
@@ -87,8 +91,8 @@ int main(int argc, char *argv[]) {
         }
 
         // Measure the time just before sending the request (Tsend)
-        struct timeval start, end;
-        gettimeofday(&start, NULL);
+      gettimeofday(&response_start,NULL);
+
 
         // Send the source code content to the server
         send(clientSocket, sourceCodeContent.c_str(), sourceCodeContent.size(), 0);
@@ -112,32 +116,40 @@ int main(int argc, char *argv[]) {
             errorRequests++;
         } else {
             // Measure the time just after getting the response (Trecv)
-            gettimeofday(&end, NULL);
-            double responseTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+            gettimeofday(&response_end, NULL);
+            double responseTime = (response_end.tv_sec - response_start.tv_sec) + (response_end.tv_usec - response_start.tv_usec) / 1000000.0;
 
             buffer[bytesRead] = '\0';
             std::cout << "Response successful: " << buffer << std::endl;
             successful_totalTime += responseTime;
             successfulResponses++;
-            successfulRequests++;
         }
 
         close(clientSocket);
         sleep(sleepTimeSeconds);
-        totalTime_of_loop += responseTime + sleepTimeSeconds;
     }
+
+ // Measure the time just after getting the response (Trecv)
+    gettimeofday(&end, NULL);
+     totalTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
 
     if (successfulResponses == 0) {
         std::cout << "Average Response Time: " << (successful_totalTime) << " seconds" << std::endl;
     } else {
         std::cout << "Average Response Time: " << (successful_totalTime / successfulResponses) << " seconds" << std::endl;
     }
+
     std::cout << "Number of Successful Responses: " << successfulResponses << std::endl;
-    std::cout << "Time Taken for Completing the Loop: " << totalTime_of_loop << " seconds" << std::endl;
+    std::cout << "Total time of the client: " << totalTime << " seconds" << std::endl;
+    std::cout << "Throughput: " << successfulResponses/totalTime << " requests/second" <<std::endl;
     printf("Total Requests Sent: %d\n", totalRequests);
-    printf("Successful Requests (Goodput): %d\n", successfulRequests);
-    printf("Timeout Requests: %d\n", timeoutRequests);
-    printf("Error Requests: %d\n", errorRequests);
+    // printf("Successful Requests (Goodput): %d\n", successfulRequests);
+   
+    //time out rate
+    std::cout << "Timeout Rate: " << timeoutRequests/totalTime << " requests/second" <<std::endl;
+    
+    //error out rate
+    std::cout << "Error Rate: " << errorRequests/totalTime << " requests/second" <<std::endl;
 
     return 0;
 }
--- Lab08/lab08_client/loadtest.sh
+++ Lab08/lab08_client/loadtest.sh
@@ -10,12 +10,13 @@ fi
 #Server_Ip="192.168.1.106"
 #Server_Ip="192.168.113.248"
 #Server_Ip="10.96.15.177";
-Server_Ip="10.130.171.214"
-#Server_Ip="127.0.0.1"
+# Server_Ip="10.130.171.214"
+Server_Ip="127.0.0.1"
 portNum="8000"
 numClients=$1
 loopNum=$2
 sleepTimeSeconds=$3
+timeOutTime=40
 
 outputDir="client_output"
 mkdir -p "$outputDir"
@@ -30,15 +31,14 @@ vmstat 1 $((loopNum + 1)) > "$outputDir/vmstat.log" &
 vmstat_pid=$!
 
 # Initialize variables for metrics
-totalRequests=0
+# totalRequests=0
 successfulRequests=0
-timeoutRequests=0
-errorRequests=0
 cpuUtilization=0
 activeThreads=0
 
+
 for ((i = 1; i <= numClients; i++)); do
-    ./gradingclient "$Server_Ip:$portNum" pass.cpp "$loopNum" "$sleepTimeSeconds" 40 > "$outputDir/client_$i.txt" &
+    ./gradingclient "$Server_Ip:$portNum" pass.cpp "$loopNum" "$sleepTimeSeconds" "$timeOutTime" > "$outputDir/client_$i.txt" &
     pids[${i}]=$! # Store the PID of each background job
 done
 
@@ -72,7 +72,12 @@ averageIdle=$(($totalIdle / lines))
 averageCPU=$(echo "100 - $averageIdle" | bc)
 
 # Calculate overall throughput and average response time
-totalThroughput=0
+throughput=0
+totalThroughput=0.0
+timeoutRate=0
+totalTimeoutRate=0.0
+errorRate=0.0
+totalErrorRate=0.0
 totalResponseTime=0
 totalSamples=0
 
@@ -80,16 +85,16 @@ for ((i = 1; i <= numClients; i++)); do
     output_file="$outputDir/client_$i.txt"
 
     # Calculate throughput (requests per second)
-    clientTime=$(grep "Average Response Time:" "$output_file" | awk '{print $(NF-1)}')
-    floatvalue=0.0
-    result=$(echo "$clientTime > $floatvalue" | bc)
-    if [ $result -eq 1 ]; then
-         throughput=$(echo "scale=2; 1 / $clientTime" | bc)
-    else
-         throughput=0
-    fi
+    # clientTime=$(grep "Average Response Time:" "$output_file" | awk '{print $(NF-1)}')
+    # floatvalue=0.0
+    # result=$(echo "$clientTime > $floatvalue" | bc)
+    # if [ $result -eq 1 ]; then
+    #      throughput=$(echo "scale=2; 1 / $clientTime" | bc)
+    # else
+    #      throughput=0
+    # fi
     
-    totalThroughput=$(echo "$totalThroughput + $throughput" | bc)
+    # totalThroughput=$(echo "$totalThroughput + $throughput" | bc)
 
     # Calculate average response time
     avgResponseTime=$(grep "Average Response Time:" "$output_file" | awk '{print $(NF-1)}')
@@ -98,20 +103,26 @@ for ((i = 1; i <= numClients; i++)); do
     totalSamples=$(echo "$totalSamples + $numSamples" | bc)
 
     # Extract and calculate metrics from gradingclient output
-    totalRequests=$(($totalRequests + $(grep "Total Requests Sent: " "$output_file" | awk '{print $NF}')))
-    successfulRequests=$(($successfulRequests + $(grep "Successful Requests (Goodput): " "$output_file" | awk '{print $NF}')))
-    timeoutRequests=$(($timeoutRequests + $(grep "Timeout Requests: " "$output_file" | awk '{print $NF}')))
-    errorRequests=$(($errorRequests + $(grep "Error Requests: " "$output_file" | awk '{print $NF}')))
+    # # totalRequests=$($totalRequests + $(grep "Total Requests Sent:"  "$output_file" | awk '{print $NF}' ) )
+    # successfulRequests=$($successfulRequests + $(grep "Successful Requests (Goodput): " "$output_file" | awk '{print $NF}'))
+    
+    throughput=$(grep "Throughput:" "$output_file" | awk '{print $(NF-1)}' )
+    totalThroughput=$(echo "scale=2; $totalThroughput + $throughput" | bc)
+    timeoutRate=$(grep "Timeout Rate:" "$output_file" | awk '{print $(NF-1)}')
+    totalTimeoutRate=$(echo "scale=2; $totalTimeoutRate + $timeoutRate" | bc)
+    errorRate=$(grep "Error Rate:" "$output_file" | awk '{print $(NF-1)}')
+    totalErrorRate=$(echo "scale=2; $totalErrorRate + $errorRate" | bc)
+    
     # ... (existing code for other metrics)
 
     # Measure CPU utilization of gradingclient
     #cpuUtilization=$(echo "scale=2; $cpuUtilization + $(top -b -n 1 -p ${pids[i]} | awk 'NR > 7 { sum += $9 } END {print sum}') / 100" | bc)
 done
 
-timeoutRequestsRate=$(echo "scale=2; $timeoutRequests / $totalRequests" | bc)
-errorRequestsRate=$(echo "scale=2; $errorRequests / $totalRequests" | bc)
-successfulRequestsRate=$(echo "scale=2; $totalThroughput / $numClients" | bc)
-RequestsSentRate=$(echo "scale=2; $totalThroughput +$timeoutRequestsRate +$errorRequestsRate" | bc)
+# timeoutRateRate=$(echo "scale=2; $timeoutRate / $totalRequests" | bc)
+# errorRateRate=$(echo "scale=2; $errorRate / $totalRequests" | bc)
+# successfulRequestsRate=$(echo "scale=2; $totalThroughput / $numClients" | bc)
+RequestsSentRate=$(echo "scale=2; $totalThroughput +$totalTimeoutRate +$totalErrorRate" | bc)
 
 # Calculate the average number of active threads
 #activeThreads=$(pgrep gradingserver | wc -l)
@@ -123,11 +134,12 @@ else
     overallAvgResponseTime=0
 fi
 
-echo "Overall Throughput: $totalThroughput requests/second"
+# echo "Overall Throughput: $totalThroughput requests/second"
 echo "Overall Average Response Time: $overallAvgResponseTime seconds"
+echo "Successful Requests Rate(Goodput): $totalThroughput requests/second"
+echo "Timeout Requests Rate: $totalTimeoutRate"
+echo "Error Requests Rate: $totalErrorRate"
 echo "Requests Sent Rate: $RequestsSentRate"
-echo "Successful Requests Rate(Goodput): $successfulRequestsRate"
-echo "Timeout Requests Rate: $timeoutRequestsRate"
-echo "Error Requests Rate: $errorRequestsRate"
 echo "Average Number of Active Threads: $activeThreads"
 echo "Overall CPU Utilization: $averageCPU%"
+
