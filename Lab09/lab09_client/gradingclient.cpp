35cd28bc1acf8f0757c7c9cd8d400d7293a92f30
VedantK11 <vedantuk11@gmail.com>
2023-11-05 22:39:03 +0530

    Updations in throughput calculation in lab09

--- Lab09/lab09_client/gradingclient.cpp
+++ Lab09/lab09_client/gradingclient.cpp
@@ -9,8 +9,10 @@
 #include <sys/select.h> // For select
 #include <errno.h>      // For errno
 
-int main(int argc, char *argv[]) {
-    if (argc != 6) {
+int main(int argc, char *argv[])
+{
+    if (argc != 6)
+    {
         std::cerr << "Usage: " << argv[0] << " <serverIP:port>  <sourceCodeFileTobeGraded>  <loopNum> <sleepTimeSeconds> <timeout-seconds>" << std::endl;
         return 1;
     }
@@ -21,18 +23,18 @@ int main(int argc, char *argv[]) {
     int sleepTimeSeconds = std::atoi(argv[4]);
     int timeoutSeconds = std::atoi(argv[5]);
 
-    double successful_totalTime = 0.0;
-    double totalTime_of_loop = 0.0;
     int successfulResponses = 0;
     double responseTime = 0;
     int totalRequests = loopNum;
-    int successfulRequests = 0;
     int timeoutRequests = 0;
     int errorRequests = 0;
+    double successful_totalTime = 0.0;
+    double totalTime = 0.0;
 
     // Extract server IP and port from the command line argument
     size_t colonPos = serverIPPort.find(':');
-    if (colonPos == std::string::npos) {
+    if (colonPos == std::string::npos)
+    {
         std::cerr << "Invalid server IP:port format" << std::endl;
         return 1;
     }
@@ -42,19 +44,24 @@ int main(int argc, char *argv[]) {
 
     // Read the content of the source code file
     std::ifstream sourceFile(sourceFileName);
-    if (!sourceFile.is_open()) {
+    if (!sourceFile.is_open())
+    {
         std::cerr << "Error opening source code file: " << sourceFileName << std::endl;
         return 1;
     }
 
-    std::string sourceCodeContent((std::istreambuf_iterator<char>(sourceFile)),
-                                  std::istreambuf_iterator<char>());
+    std::string sourceCodeContent((std::istreambuf_iterator<char>(sourceFile)), std::istreambuf_iterator<char>());
+
+    struct timeval start, end, response_start, response_end;
+    gettimeofday(&start, NULL);
 
     // Perform the request-response loop
-    for (int i = 0; i < loopNum; i++) {
+    for (int i = 0; i < loopNum; i++)
+    {
         // Create a socket
         int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-        if (clientSocket == -1) {
+        if (clientSocket == -1)
+        {
             perror("Socket creation error");
             return 1;
         }
@@ -63,7 +70,8 @@ int main(int argc, char *argv[]) {
         struct timeval timeout;
         timeout.tv_sec = timeoutSeconds;
         timeout.tv_usec = 0;
-        if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
+        if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1)
+        {
             perror("Set receive timeout error");
             close(clientSocket);
             return 1;
@@ -73,13 +81,15 @@ int main(int argc, char *argv[]) {
         struct sockaddr_in serverAddress;
         serverAddress.sin_family = AF_INET;
         serverAddress.sin_port = htons(port);
-        if (inet_pton(AF_INET, serverIP.c_str(), &serverAddress.sin_addr) <= 0) {
+        if (inet_pton(AF_INET, serverIP.c_str(), &serverAddress.sin_addr) <= 0)
+        {
             perror("Invalid server address");
             close(clientSocket);
             return 1;
         }
 
-        if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
+        if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+        {
             perror("Connection error");
             close(clientSocket);
             errorRequests++;
@@ -87,8 +97,7 @@ int main(int argc, char *argv[]) {
         }
 
         // Measure the time just before sending the request (Tsend)
-        struct timeval start, end;
-        gettimeofday(&start, NULL);
+        gettimeofday(&response_start, NULL);
 
         // Send the source code content to the server
         send(clientSocket, sourceCodeContent.c_str(), sourceCodeContent.size(), 0);
@@ -98,47 +107,62 @@ int main(int argc, char *argv[]) {
         ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
 
         // Check for a receive timeout error
-        if (bytesRead == -1) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        if (bytesRead == -1)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+            {
                 std::cout << "Request timeout" << std::endl;
                 timeoutRequests++;
-            } else {
+            }
+            else
+            {
                 perror("Error receiving response from server");
                 errorRequests++;
             }
-        } else if (bytesRead == 0) {
+        }
+        else if (bytesRead == 0)
+        {
             // Connection closed by the server
             std::cout << "Connection closed by the server" << std::endl;
             errorRequests++;
-        } else {
+        }
+        else
+        {
             // Measure the time just after getting the response (Trecv)
-            gettimeofday(&end, NULL);
-            double responseTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+            gettimeofday(&response_end, NULL);
+            double responseTime = (response_end.tv_sec - response_start.tv_sec) + (response_end.tv_usec - response_start.tv_usec) / 1000000.0;
 
             buffer[bytesRead] = '\0';
             std::cout << "Response successful: " << buffer << std::endl;
             successful_totalTime += responseTime;
             successfulResponses++;
-            successfulRequests++;
         }
 
         close(clientSocket);
         sleep(sleepTimeSeconds);
-        totalTime_of_loop += responseTime + sleepTimeSeconds;
     }
+    gettimeofday(&end, NULL);
+    totalTime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
 
-    if (successfulResponses == 0) {
+    if (successfulResponses == 0)
+    {
         std::cout << "Average Response Time: " << (successful_totalTime) << " seconds" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Average Response Time: " << (successful_totalTime / successfulResponses) << " seconds" << std::endl;
     }
+
     std::cout << "Number of Successful Responses: " << successfulResponses << std::endl;
-    std::cout << "Time Taken for Completing the Loop: " << totalTime_of_loop << " seconds" << std::endl;
+    std::cout << "Total time of the client: " << totalTime << " seconds" << std::endl;
+    std::cout << "Throughput: " << successfulResponses / totalTime << " requests/second" << std::endl;
     printf("Total Requests Sent: %d\n", totalRequests);
-    printf("Successful Requests (Goodput): %d\n", successfulRequests);
-    printf("Timeout Requests: %d\n", timeoutRequests);
-    printf("Error Requests: %d\n", errorRequests);
+
+    // time out rate
+    std::cout << "Timeout Rate: " << timeoutRequests / totalTime << " requests/second" << std::endl;
+
+    // error rate
+    std::cout << "Error Rate: " << errorRequests / totalTime << " requests/second" << std::endl;
 
     return 0;
 }
-
